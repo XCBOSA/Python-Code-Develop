4bc6ff0a60b690d37df3d0446008c69aaaadd5bc
xcbosa mbp16 <xcbosa@forgetive.org>
2025-03-02 01:27:53 +0800

    [Arch] Add postbuild script

---
+++ postbuild.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+if [ -d output/images/rootfs ]; then
+	rm -rf output/images/rootfs
+fi
+mkdir output/images/rootfs
+rm -rf output/images/rootfs.tar
+tar -vxjf output/images/rootfs.tar.bz2 -C output/images/rootfs
+
+GCC="output/host/bin/riscv64-buildroot-linux-gnu-gcc"
+
+for file in `ls post_sbin`
+    do
+        filePath="post_sbin/"$file
+        if [ -f $filePath ]
+        then
+            gccout="output/images/rootfs"${file%".c"}
+            $GCC $filePath -o $gccout -O2
+        fi
+    done
+tar -vcjf output/images/rootfs.tar -C output/images/rootfs/*
