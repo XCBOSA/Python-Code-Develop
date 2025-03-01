cec0981700cad99c47ffb60b49c5622651bbc6d6
xcbosa mbp16 <xcbosa@forgetive.org>
2025-03-02 01:32:09 +0800

    [Arch] Add postbuild script

--- postbuild.sh
+++ postbuild.sh
@@ -11,9 +11,11 @@ GCC="output/host/bin/riscv64-buildroot-linux-gnu-gcc"
 for file in `ls post_sbin`
     do
         filePath="post_sbin/"$file
-        if [ -f $filePath ]
-        then
+        if [ -f $filePath ]; then
             gccout="output/images/rootfs"${file%".c"}
+            if [ -f $gccout ]; then
+                rm $gccout
+            fi
             $GCC $filePath -o $gccout -O2
         fi
     done
